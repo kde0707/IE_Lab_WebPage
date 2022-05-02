886e8b6116255bd26ee0c58aadf981ae3cb41a31
seokii <dev.seokii@gmail.com>
2022-05-02 15:09:39 +0900

    Add ListView,DetailView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/single_post_page.html'
