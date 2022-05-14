48685604dec663567825ead5cb10cdace959c80b
seokii <dev.seokii@gmail.com>
2022-05-14 20:47:15 +0900

    Add create_post auth 2022-05-14

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.shortcuts import render, redirect
 
@@ -15,13 +15,16 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'blog/blog_detail.html'
 
-class PostCreate(LoginRequiredMixin, CreateView):
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload']
 
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
     def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated:
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
             return super(PostCreate, self).form_valid(form)
         else:
