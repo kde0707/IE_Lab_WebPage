64f3d930205ef5086613830b9276006a9112fa51
seokii <dev.seokii@gmail.com>
2022-05-15 00:36:27 +0900

    Add MarkdownX 2022-05-15

--- IE_LAB_WEB/urls.py
+++ IE_LAB_WEB/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('markdownx/', include('markdownx.urls')),
     path('blog/', include('blog.urls')),
     path('', include('pages.urls')),
 ]
