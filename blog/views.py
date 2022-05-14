5b06f4157dc947e320457ecfc1a5a21e4b68cc69
seokii <dev.seokii@gmail.com>
2022-05-14 20:33:58 +0900

    Add CreatePostPage 2022-05-14

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -12,3 +14,15 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     template_name = 'blog/blog_detail.html'
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
