7dac20863fcd2ab32e3bc7b8848a24e860105f09
seokii <dev.seokii@gmail.com>
2022-08-28 22:37:18 +0900

    2022-08-28_3

--- IE_LAB_WEB/settings.py
+++ IE_LAB_WEB/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 # else:
 #     ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost', '127.0.0.1', '210.119.104.132']
 
 
 # Application definition
