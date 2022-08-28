469c3372c0f9cef1d6c1002ba9ba614bd0de9f9e
seokii <dev.seokii@gmail.com>
2022-08-28 22:34:45 +0900

    2022-08-28_2

--- IE_LAB_WEB/settings.py
+++ IE_LAB_WEB/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j9uasby&*rq=qto04sw2xvr7#3%)=kmcrgz_#k3j3kjp=8f7qy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = int(os.environ.get('DEBUG', 1))
-
-if os.environ.get('DJANGO_ALLOWED_HOSTS'):
-    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
-else:
-    ALLOWED_HOSTS = []
+DEBUG = True
+# DEBUG = int(os.environ.get('DEBUG', 1))
+#
+# if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+#     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+# else:
+#     ALLOWED_HOSTS = []
 
 ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost', '127.0.0.1']
 
