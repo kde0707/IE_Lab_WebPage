852cb9214841597a22702314ff328fe4e5a2da6b
seokii <dev.seokii@gmail.com>
2022-09-05 13:00:08 +0900

    2022-09-05

--- IE_LAB_WEB/settings.py
+++ IE_LAB_WEB/settings.py
@@ -131,6 +131,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join('static/')
+# )
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
